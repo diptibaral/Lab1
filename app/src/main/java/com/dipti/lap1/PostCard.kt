bef1138bb921c6d3c64e91822ae88798c8d2f49c
[Dipti Baral] <[premaswoorupa@gmail.com]>
2020-12-08 13:14:38 +0545

    Lab1

---
+++ app/src/main/java/com/dipti/lap1/PostCard.kt
@@ -0,0 +1,11 @@
+package com.dipti.lap1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_card)
+    }
+}
